f0f638b1071c71f59ec042ab2149241132134079
Ricky Beltran <rickynel2001@gmail.com>
2025-03-26 14:28:55 -0500

    code webrtc separated

--- src/App.jsx
+++ src/App.jsx
@@ -1,199 +1,186 @@
-import "./App.css";
-import { useEffect, useState, useRef } from "react";
+import './App.css'
+import { useEffect, useState, useRef } from 'react'
+import { useWebSocket } from './hooks/useWebSocket/useWebSocket'
+import { CloseCallIcon } from './icons'
+import { useWebRTC } from './hooks/useWebRTC/useWebRTC'
 
 function App() {
-  const [isSocketConnected, setIsSocketConnected] = useState(false);
-  const [mute, setMute] = useState(false);
-  const [video, setVideo] = useState(false);
-  const localStream = useRef();
-  const webSocket = useRef();
-  const pc = useRef();
-  const videoLocal = useRef();
-  const videoRemote = useRef();
+	const [showVideoChannel, setShowVideoChannel] = useState(false)
+	const [isSocketConnected, setIsSocketConnected] = useState(false)
+	const [isMuteCall, setIsMuteCall] = useState(false)
+	const [isShowCamera, setIsShowCamera] = useState(false)
 
-  useEffect(() => {
-    if (webSocket.current?.readyState !== 1) {
-      webSocket.current = new WebSocket(
-        "wss://meet.estoesunaprueba.fun:8050/ws/webrtc/"
-      );
+	const localStream = useRef()
+	const videoLocal = useRef()
+	const videoRemote = useRef()
 
-      webSocket.current.onopen = (event) => {
-        console.log("socket conectado", event);
-        setIsSocketConnected(true);
-      };
+	const { webSocket, createWebSocket, socketSendMessage } = useWebSocket()
+	const {
+		peerConnection,
+		handlerMessagesWebRTC,
+		createOffer,
+		closeConnection,
+	} = useWebRTC()
 
-      webSocket.current.onmessage = (event) => {
-        console.log(event.data);
-        conexionPeer(event.data);
-      };
+	useEffect(() => {
+		if (webSocket?.readyState !== webSocket?.OPEN) {
+			const onopen = (event) => {
+				console.log('socket conectado', event)
+				setIsSocketConnected(true)
+			}
 
-      webSocket.current.onclose = (event) => {
-        console.log("socket cerrado", event);
-        setIsSocketConnected(false);
-        pc.current.close();
-      };
+			const onmessage = (event) => {
+				console.log(event.data)
+				handlerMessagesWebRTC(event.data)
+			}
 
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: true,
-          video: true,
-        })
-        .then((stream) => {
-          localStream.current = stream;
-          videoLocal.current.srcObject = stream;
-        });
-    }
-  }, []);
+			const onclose = (event) => {
+				console.log('socket cerrado', event)
+				setIsSocketConnected(false)
+				closeConnection()
+			}
 
-  const sendMessage = () => {
-    if (webSocket.current?.readyState === 1) {
-      console.log("enviando mensaje...");
-      webSocket.current.send(JSON.stringify({ message: "Hola Perra sarnosa" }));
-    }
-  };
+			createWebSocket({
+				url: 'wss://meet.estoesunaprueba.fun:8050/ws/webrtc/',
+				onopen,
+				onmessage,
+				onclose,
+			})
+		}
+	}, [])
 
-  const startVideo = async () => {
-    await crearPeer();
-    const offer = await pc.current.createOffer();
-    await pc.current.setLocalDescription(offer);
-    webSocket.current.send(JSON.stringify(offer));
-  };
+	// const sendMessage = () => {
+	// 	if (webSocket?.readyState === webSocket?.OPEN) {
+	// 		console.log('enviando mensaje...')
+	// 		socketSendMessage({ message: 'Hola Perra sarnosa' })
+	// 	}
+	// }
 
-  function conexionPeer(dataPeer) {
-    const dataParsed = JSON.parse(dataPeer);
-    switch (dataParsed.type) {
-      case "offer":
-        manejarOferta(dataParsed);
-        break;
-      case "answer":
-        manejarRespuesta(dataParsed);
-        break;
-      case "candidate":
-        manejarCandidato(dataParsed);
-        break;
-      case "closeCall":
-        closeCall();
-        break;
-      default:
-        console.log("opción inválida");
-        break;
-    }
-  }
+	const startVideo = async () => {
+		console.log('iniciar videollamada')
+		const handlerOnTrack = (event) => {
+			console.log({ event })
+			videoRemote.current.srcObject = event.streams[0]
+		}
 
-  async function crearPeer() {
-    const configuracion = {
-      iceServers: [
-        {
-          urls: "stun:stun.l.google.com:19302",
-        },
-      ],
-    };
+		createOffer({ handlerOnTrack })
+		localStream.current
+			.getTracks()
+			.forEach((track) => peerConnection.addTrack(track, localStream.current))
+	}
 
-    pc.current = new RTCPeerConnection(configuracion);
+	const handlerCloseCall = () => {
+		closeConnection()
+		setShowVideoChannel(false)
+		socketSendMessage({ type: 'closeCall' })
+	}
 
-    pc.current.ontrack = (event) => {
-      console.log({ event });
-      videoRemote.current.srcObject = event.streams[0];
-      // event.streams[0].getTracks().forEach(track => {
-      // 	videoRemote.current.srcObject = track
-      // })
-    };
+	const handlerMuteCall = () => {
+		setIsMuteCall((prev) => !prev)
 
-    pc.current.onicecandidate = (event) => {
-      if (event.candidate) {
-        webSocket.current.send(
-          JSON.stringify({ type: "candidate", candidate: event.candidate })
-        );
-      }
-    };
+		const tracks = localStream.current.getAudioTracks()
+		tracks.forEach((track) => (track.enabled = isMuteCall))
+	}
 
-    localStream.current
-      .getTracks()
-      .forEach((track) => pc.current.addTrack(track, localStream.current));
-  }
+	const handlerShowCall = () => {
+		setIsShowCamera((prev) => !prev)
 
-  async function manejarOferta(offer) {
-    await crearPeer();
-    await pc.current.setRemoteDescription(new RTCSessionDescription(offer));
-    const answer = await pc.current.createAnswer();
-    await pc.current.setLocalDescription(answer);
-    webSocket.current.send(JSON.stringify(answer));
-  }
+		const tracks = localStream.current.getVideoTracks()
+		tracks.forEach((track) => (track.enabled = isShowCamera))
+	}
 
-  async function manejarRespuesta(answer) {
-    if (pc.current.signalingState !== "stable") {
-      await pc.current.setRemoteDescription(new RTCSessionDescription(answer));
-    }
-  }
+	const joinVideoChannel = () => {
+		setShowVideoChannel(true)
 
-  async function manejarCandidato(candidato) {
-    if (candidato.candidate) {
-      const iceCandidate = new RTCIceCandidate(candidato.candidate);
-      await pc.current.addIceCandidate(iceCandidate);
-    }
-  }
+		navigator.mediaDevices
+			.getUserMedia({
+				audio: true,
+				video: true,
+			})
+			.then((stream) => {
+				localStream.current = stream
+				videoLocal.current.srcObject = stream
+				startVideo()
+			})
+			.catch((error) => {
+				alert('no tiene camara')
+				console.error(error)
+			})
+	}
 
-  function closeCall() {
-    pc.current.close();
-  }
+	return (
+		<main>
+			<aside>
+				<div>
+					<ul className='list-channels'>
+						<li>
+							<button onClick={joinVideoChannel} className='btnJoinChannel'>
+								canal de video
+							</button>
+						</li>
+					</ul>
+				</div>
+			</aside>
 
-  function handlerCloseCall() {
-    closeCall();
-    webSocket.current.send(JSON.stringify({ type: "closeCall" }));
-  }
+			{showVideoChannel && (
+				<section>
+					<div>
+						<div>
+							<video ref={videoLocal} autoPlay muted></video>
+							<video ref={videoRemote} autoPlay></video>
+						</div>
 
-  function muteCall() {
-    setMute((prev) => !prev);
+						<div>
+							{/* <button onClick={startVideo} disabled={!isSocketConnected}>
+								iniciar llamada
+							</button> */}
+							{/* <button
+								onClick={sendMessage}
+								disabled={!isSocketConnected}
+								className='btnSendMessage'
+							>
+								enviar mensaje
+							</button> */}
+							<button onClick={handlerCloseCall} disabled={!isSocketConnected}>
+								<CloseCallIcon />
+							</button>
+							<button onClick={handlerMuteCall} disabled={!isSocketConnected}>
+								{isMuteCall ? 'desmutear' : 'mutear'}
+							</button>
+							<button onClick={handlerShowCall} disabled={!isSocketConnected}>
+								{isShowCamera ? <ShowCameraIcon /> : <HideShowCameraIcon />}
+							</button>
+						</div>
+					</div>
+				</section>
+			)}
 
-    const tracks = localStream.current.getAudioTracks();
-    tracks.forEach((track) => (track.enabled = mute));
-  }
+			{/* <button onClick={startVideo} disabled={!isSocketConnected}>
+				iniciar llamada
+			</button>
+			<button
+				onClick={sendMessage}
+				disabled={!isSocketConnected}
+				className='btnSendMessage'
+			>
+				enviar mensaje
+			</button>
+			<button onClick={handlerCloseCall} disabled={!isSocketConnected}>
+				terminar llamada
+			</button>
+			<button onClick={muteCall} disabled={!isSocketConnected}>
+				{mute ? 'desmutear' : 'mutear'}
+			</button>
+			<button onClick={showCall} disabled={!isSocketConnected}>
+				{video ? 'ver video' : 'quitar video'}
+			</button>
 
-  function showCall() {
-    setVideo((prev) => !prev);
-
-    const tracks = localStream.current.getVideoTracks();
-    tracks.forEach((track) => (track.enabled = video));
-  }
-
-  return (
-    <div>
-      <p>Hola</p>
-
-      <button onClick={startVideo} disabled={!isSocketConnected}>
-        iniciar llamada
-      </button>
-      <button
-        onClick={sendMessage}
-        disabled={!isSocketConnected}
-        className="btnSendMessage"
-      >
-        enviar mensaje
-      </button>
-      <button onClick={handlerCloseCall} disabled={!isSocketConnected}>
-        terminar llamada
-      </button>
-      <button onClick={muteCall} disabled={!isSocketConnected}>
-        {mute ? "desmutear" : "mutear"}
-      </button>
-      <button onClick={showCall} disabled={!isSocketConnected}>
-        {video ? "ver video" : "quitar video"}
-      </button>
-      <button onClick={startVideo}>iniciar llamada</button>
-      <button
-        onClick={sendMessage}
-        disabled={!isSocketConnected}
-        className="btnSendMessage"
-      >
-        send message
-      </button>
-
-      <div>
-        <video ref={videoLocal} autoPlay muted></video>
-        <video ref={videoRemote} autoPlay></video>
-      </div>
-    </div>
-  );
+			<div>
+				<video ref={videoLocal} autoPlay muted></video>
+				<video ref={videoRemote} autoPlay></video>
+			</div> */}
+		</main>
+	)
 }
 
-export default App;
+export default App
---
+++ src/icons.jsx
@@ -0,0 +1,50 @@
+export const ShowCameraIcon = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='size-6'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z'
+		/>
+	</svg>
+)
+
+export const HideShowCameraIcon = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='size-6'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M12 18.75H4.5a2.25 2.25 0 0 1-2.25-2.25V9m12.841 9.091L16.5 19.5m-1.409-1.409c.407-.407.659-.97.659-1.591v-9a2.25 2.25 0 0 0-2.25-2.25h-9c-.621 0-1.184.252-1.591.659m12.182 12.182L2.909 5.909M1.5 4.5l1.409 1.409'
+		/>
+	</svg>
+)
+
+export const CloseCallIcon = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='size-6'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='M15.75 3.75 18 6m0 0 2.25 2.25M18 6l2.25-2.25M18 6l-2.25 2.25m1.5 13.5c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z'
+		/>
+	</svg>
+)
