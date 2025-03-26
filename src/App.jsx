df514ce09872d4e9ad5f91157f8a0b15574b0bf0
Ricky Beltran <rickynel2001@gmail.com>
2025-03-26 16:08:05 -0500

    Revert "logs"
    This reverts commit 4f57c2dec0f6590ccd0a0b9f7319824185cfebfd.

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     };
 
     const onmessage = (event) => {
-      console.log(event);
+      console.log(event.data);
       handlerMessagesWebRTC(event.data);
     };
 
--- src/hooks/useWebRTC/useWebRTC.js
+++ src/hooks/useWebRTC/useWebRTC.js
@@ -23,7 +23,6 @@ export function useWebRTC({ handlerOnTrack, socketSendMessage }) {
 
   const handlerMessagesWebRTC = (dataPeer) => {
     const dataParsed = JSON.parse(dataPeer);
-    console.log({ dataParsed });
     switch (dataParsed.type) {
       case "offer":
         manejarOferta(dataParsed);
